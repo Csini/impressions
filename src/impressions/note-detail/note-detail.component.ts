a934cbd04e473f9bd04b19135ea80342f03a564b
csini <csini@gmx.com>
2024-11-09 11:48:29 +0100

    ISSUE-6: note-detail insert and update works

--- src/impressions/note-detail/note-detail.component.ts
+++ src/impressions/note-detail/note-detail.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Input as RouterInput } from '@angular/core';
 import { LocalService } from '../common/local.service';
 import { Note } from '../note/note.model';
-import { FormsModule } from '@angular/forms';
-import { NgFor } from '@angular/common';
+import { JsonPipe, NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'impressions-note-detail',
   standalone: true,
-  imports: [FormsModule, NgFor],
+  imports: [RouterLink, NgFor, ReactiveFormsModule, JsonPipe],
   templateUrl: './note-detail.component.html',
   styleUrl: './note-detail.component.scss'
 })
@@ -18,30 +19,79 @@ export class NoteDetailComponent implements OnInit {
 
   note: Note = new Note();
 
-  constructor(private localService: LocalService) {
+  noteDetailForm!: FormGroup;
 
+  @ViewChild('newRowInput')
+  newRowInput!: ElementRef;
+
+  constructor(private localService: LocalService, private changeDetector: ChangeDetectorRef, private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     console.log('NoteDetailComponent: ' + this.id);
-    this.note =this.localService.findData(this.id);
+
+    this.noteDetailForm = new FormGroup({
+      newRow: new FormControl(''),
+      rows: this.fb.array([]),
+    });
+
+    this.reloadNote();
   }
 
+  get rows(): FormArray {
+    return this.noteDetailForm.get("rows") as FormArray
+  }
 
-  content = ""
-  addBulletText(event: KeyboardEvent) {
- 
-    if (event.code === "Enter") {
-      this.content += '• '
+  rowValue (index: number): string {
+    return this.rows.at(index).value.row;
+  }
+
+  get newRow(): FormControl {
+    return this.noteDetailForm.get('newRow') as FormControl;
+  }
+
+  onFocusOutRowEvent($event: FocusEvent, index: number) {
+    console.log("onFocusOutRowEvent[" + JSON.stringify($event) + ", " + index + "]");
+
+    let newValue : string = this.rowValue(index);
+    console.log("newValue:" + newValue);
+
+    this.note.rows[index] = newValue;
+
+    this.localService.changeData(this.note);
+    this.reloadNote();
+  }
+
+  onFocusOutNewRowEvent($event: FocusEvent) {
+    console.log("onFocusOutNewRowEvent[" + this.newRow?.value + "]")
+    if (this.newRow?.value && this.newRow?.value != '') {
+      this.note.rows.push(this.newRow?.value);
+      this.localService.changeData(this.note);
+      this.reloadNote();
+      this.newRow?.setValue('');
     }
-
-    this.content = this.content.replace(/[\r|\n|\r\n]$/, '');
   }
 
-  mytextOnFocus() {
-    if(this.content.length==0){
-      this.content += '• ';
+  reloadNote() {
+    this.note = this.localService.findData(this.id);
+    let counter: number = 0;
+
+    this.rows.clear();
+
+    this.note.rows.forEach(rowItem => {
+      let group: FormGroup = new FormGroup({
+        row: new FormControl(rowItem)
+      });
+      this.rows.push(group);
     }
+    );
 
+    this.changeDetector.detectChanges();
+    this.newRowInput.nativeElement.focus();
   }
+
+  submitForm() {
+    throw new Error('Method not implemented.');
+  }
+
 }
