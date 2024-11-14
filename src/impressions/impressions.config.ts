c751d7697025b93ff91a7b00dedeb0f01f55e9b3
csini <csini@gmx.com>
2024-11-14 10:54:28 +0100

    ISSUE-16: NGXLogger

--- src/impressions/impressions.config.ts
+++ src/impressions/impressions.config.ts
@@ -1,17 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './impressions.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 export const impressionsConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
+    provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    importProvidersFrom(LoggerModule.forRoot({
+      serverLoggingUrl: '/api/logs',
+      level: isDevMode() ? NgxLoggerLevel.DEBUG : NgxLoggerLevel.INFO,
+      serverLogLevel: NgxLoggerLevel.ERROR
+    })),
   ]
 };
